b489e821862cc884d3a723b17a88640b1b1b75ab
zhiyongzaixian <13852292775@163.com>
2020-08-13 10:43:55 +0800

    '0813'

---
+++ uniapp_study/guigushangcheng_study/pages/index/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="indexContainer">
+		<!-- 头部区域  -->
+		<view class="header">
+			<image class="logo" src="../../static/images/logo.png" mode=""></image>
+			<view class="search">
+				<input type="text" value="" placeholder="搜索商品" placeholder-class="placeholder"/>
+			</view>
+			<button class="userName">志勇在线</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+<style lang="stylus">
+	.indexContainer
+		.header
+			display flex
+			height 60rpx
+			padding 10rpx
+			.logo
+				width 140rpx
+				height 40rpx
+				margin 10rpx 30rpx
+			.search
+				height 60rpx
+				width 420rpx
+				background #ededed
+				margin 0 10rpx
+				border-radius 10rpx
+				input
+					height 60rpx
+				.placeholder
+					font-size 24rpx
+			button
+				width 144rpx
+				height 60rpx
+				font-size 22rpx
+				color #BB2C08
+				text-align center
+				line-height 60rpx
+				white-space nowrap
+				overflow hidden
+				text-overflow ellipsis
+			
+	
+	
+	
+.test
+	font-size 0
+</style>
---
+++ uniapp_study/guigushangcheng_study/pages/index/stylus样式测试.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="indexContainer">
+		123
+		<p>测试内容</p>
+		<div>div测试内容</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="stylus">
+	// 常规样式
+	// * {
+	// 	padding: 0;
+	// }
+	
+	/*
+	stylus： 语法
+		1. 省略大括号
+		2. 省略冒号，封号
+		3. 样式层级可以嵌套，层级同对应结构层级一致，方便查看
+	
+	
+	*/ 
+	.indexContainer
+		p
+			font-size 40rpx
+			color red
+		div
+			color green
+	
+	
+	
+.test
+	font-size 0
+</style>
