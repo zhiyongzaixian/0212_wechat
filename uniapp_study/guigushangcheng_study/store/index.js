f87c0e67ceaa756a8951359ae6bee7d11f2c2bd6
zhiyongzaixian <13852292775@163.com>
2020-08-14 16:53:10 +0800

    '0814'

--- uniapp_study/guigushangcheng_study/store/index.js
+++ uniapp_study/guigushangcheng_study/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import indexModule from './modules/index.js'
+import cartModule from './modules/cart.js'
 // 声明使用Vue的扩展库
 
 // Vue.use都干了什么事情？？
@@ -8,7 +9,8 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
 	modules: {
-		indexModule
+		indexModule,
+		cartModule
 	}
 })
 
