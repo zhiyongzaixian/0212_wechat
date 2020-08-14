34bff80ffd5753271e7a22ee31c88e8add8565fb
zhiyongzaixian <13852292775@163.com>
2020-08-14 14:31:52 +0800

    '0814'\

--- uniapp_study/guigushangcheng_study/pages/cateGory/cateGory.vue
+++ uniapp_study/guigushangcheng_study/pages/cateGory/cateGory.vue
@@ -16,7 +16,15 @@
 				</scroll-view>
 			</view>
 			<view class="rightContainer">
-				右侧
+				<scroll-view scroll-y="true" class="rightScroll">
+					<view class="proInfo">
+						<image class="proImg" :src="cateGoryObj.imgUrl" mode=""></image>
+						<view class="proItem" v-for="proItem in cateGoryObj.subCateList" :key='proItem.id'>
+							<image :src="proItem.wapBannerUrl" mode=""></image>
+							<view>{{proItem.name}}</view>
+						</view>
+					</view>
+				</scroll-view>
 			</view>
 		</view>
 	</view>
@@ -43,6 +51,12 @@
 			changeNavId(navId){
 				this.navId = navId
 			}
+		},
+		computed: {
+			cateGoryObj(){
+				// find filter forEach
+				return this.cateGoryList.find(item => item.id === this.navId)
+			}
 		}
 	}
 </script>
@@ -63,10 +77,15 @@
 		.contentContainer		
 			height calc(100vh - 80rpx)
 			display flex
+			box-sizing border-box
+			border-top 1rpx solid #eee
 			.leftContainer
 				width 20%
+				height 100%
+				border-right 1rpx solid #eee
+				box-sizing border-box
 				.leftScroll
-					height calc(100vh - 80rpx)
+					height 100%
 					.navItem
 						position relative
 						font-size 24rpx
@@ -81,10 +100,27 @@
 							width 2rpx
 							height 60rpx
 							background #BB2C08
-							
 			.rightContainer
 				width 80%
-				background #41A863
+				height 100%
+				.rightScroll
+					height 100%
+					.proInfo
+						display flex
+						flex-wrap wrap
+						.proImg 
+							width 520rpx
+							height 190rpx
+							margin 20rpx auto
+						.proItem
+							width 33.33%
+							image 
+								width 90%
+								height 140rpx
+							view
+								font-size 26rpx
+								text-align center
+					
 		
 .test
 	font-size 0
--- uniapp_study/guigushangcheng_study/unpackage/dist/dev/mp-weixin/pages/cateGory/cateGory.js
+++ uniapp_study/guigushangcheng_study/unpackage/dist/dev/mp-weixin/pages/cateGory/cateGory.js
@@ -147,6 +147,14 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 
 
 
+
+
+
+
+
+
+
+
 var _request = _interopRequireDefault(__webpack_require__(/*! ../../utils/request.js */ 21));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};}var _default =
 {
   data: function data() {
@@ -166,6 +174,12 @@ var _request = _interopRequireDefault(__webpack_require__(/*! ../../utils/reques
 
     changeNavId: function changeNavId(navId) {
       this.navId = navId;
+    } },
+
+  computed: {
+    cateGoryObj: function cateGoryObj() {var _this = this;
+      // find filter forEach
+      return this.cateGoryList.find(function (item) {return item.id === _this.navId;});
     } } };exports.default = _default;
 
 /***/ }),
